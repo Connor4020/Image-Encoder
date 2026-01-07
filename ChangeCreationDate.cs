27af8ed5e5f084e9a522e9c4d090a60a2958ea3f
Connor4020 <155911089+BC341@users.noreply.github.com>
2026-01-07 16:56:00 +0000

    Improved and error checked ChangeCreationDate

--- ChangeCreationDate.cs
+++ ChangeCreationDate.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Globalization;
 using System.Linq;
 using System.Runtime.Serialization;
 using System.Text;
@@ -39,52 +40,22 @@ namespace Barton___Y2_Project
             if (decisionInt == 1)
             {
                 ConsoleHelper.PrintConsoleBlock("Please input your custom date and time in the format: DD/MM/YYYY HH:MM:SS\n(Please not that dates in the future won't work):", true);
+                string format = "dd/MM/yyyy HH:mm:ss";
                 string customDT = Console.ReadLine();
-                while (!DateTime.TryParse(customDT, out _))
+                DateTime newDate;
+                while (!DateTime.TryParseExact(customDT, format, CultureInfo.InvariantCulture, DateTimeStyles.None, out newDate))
                 {
                     ConsoleHelper.PrintConsoleBlock("Invalid format, ensure dashes for the date and colons for the time:", true);
                     customDT = Console.ReadLine();
                 }
-                SetCustomDate(DateTime.Parse(customDT));
+                File.SetCreationTime(fileLoc, newDate);
+                ConsoleHelper.PrintConsoleBlock($"The file at {fileLoc} has had their data and time changed to {newDate}.", false);
             }
             if (decisionInt == 2)
             {
-                SetCurrentDate(DateTime.Now);
+                File.SetCreationTime(fileLoc, DateTime.Now);
+                ConsoleHelper.PrintConsoleBlock($"The file at {fileLoc} has had their date and time changed to {DateTime.Now}.", false);
             }
-        }
-
-
-
-        private static void SetCurrentDate(DateTime date)
-        {
-            File.SetCreationTime(fileLoc, DateTime.Now);
-            ConsoleHelper.PrintConsoleBlock($"The file at {fileLoc} has had their date and time changed to {DateTime.Now}.", false);
-            ConsoleHelper.ReturnToMenuPrompt();
-        }
-
-
-
-        private static void SetCustomDate(DateTime dateTime)
-        {
-            // Made this a sb in case I ever wanna add more stuff.
-            StringBuilder sb = new StringBuilder();
-            sb.AppendLine("Please input your custom date & time in the following format: DD/MM/YYYY HH:MM:SS.\n");
-            sb.AppendLine("(Please note that dates in the future won't work).");
-
-            // Formatting needed cos of sb.
-            ConsoleHelper.PrintDivider();
-            Console.Write(sb.ToString());
-
-
-            ConsoleHelper.PrintConsoleBlock(sb.ToString(), true);
-            string customDT = Console.ReadLine();
-            while (!DateTime.TryParse(customDT, out _))
-            {
-                ConsoleHelper.PrintConsoleBlock("Invalid format, please try again:", true);
-                customDT = Console.ReadLine();
-            }
-            File.SetCreationTime(fileLoc, DateTime.Parse(customDT));
-            ConsoleHelper.PrintConsoleBlock($"The file at {fileLoc} has had their data and time changed to {customDT}.", false);
             ConsoleHelper.ReturnToMenuPrompt();
         }
     }
